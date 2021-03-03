b5fef3890c44f463b9db3e1f484a5088c2d97a09
Larry Junior <larry@bizcommerce.com.br>
2021-03-03 05:23:10 -0300

    1 try to change colors

--- src/app/code/Newmodule/Changecollors/console/changecollor.php
+++ src/app/code/Newmodule/Changecollors/console/changecollor.php
@@ -62,24 +62,41 @@ class Changecollor extends Command
         //Since it requires a VALUE_REQUIRED to come to execute() I will no put an if
         //To verify if $color or $store are inputted
 
-        try {
-            $color = $input->getOption(self::COLOR);
-            $store = $input->getOption(self::STORE);
+        $color = $input->getOption(self::COLOR);
+        $store = $input->getOption(self::STORE);
 
-            $m = "";
+        $m = "";
 
-            ctype_xdigit($color) ? (
-                is_numeric($store) ? "" :
-                    $m = 'storeid must be numeric'
-            ) : $m = 'hex must an hex value';
+        ctype_xdigit($color) ? (
+            is_numeric($store) ? "" :
+                $m = 'storeid must be numeric'
+        ) : $m = 'hex must an hex value';
 
-            if(!empty($m))
-                $output->writeln($m);
-            else {
-                $output->writeln($this->getStoreData()['label']);
+        if(!empty($m))
+            $output->writeln($m);
+        else {
+            $storeManager = \Magento\Framework\App\ObjectManager::getInstance()
+                ->get(\Magento\Store\Model\StoreManagerInterface::class);
+
+            $ST = $storeManager->getStore($store);
+
+            $array = [];
+            $array['button__color'] = '#'.$color;
+            $array['button__background'] ='#'.$color;
+
+            try {
+                $om = \Magento\Framework\App\ObjectManager::getInstance();
+                $filesystem = $om->get('Magento\Framework\Filesystem');
+                $directoryList = $om->get('Magento\Framework\App\Filesystem\DirectoryList');
+                $media = $filesystem->getDirectoryWrite($directoryList::MEDIA);
+                $contents = "ddddddddddddddddddddddddddd";
+                $media->writeFile("module1/sample.txt",$contents);
+            } catch(Exception $e) {
+                echo $e->getMessage();
             }
-        } catch (\Exception $e) {
-            $output->writeln($e);
+
+
         }
     }
+
 }
